4d325f6b8f93842c8beefd566fe594ca999b23c1
cris7M <krishnasanjay010@gmail.com>
2025-03-29 20:56:25 +0530

    note app basic assignment v1

--- dpa-backend/app.py
+++ dpa-backend/app.py
@@ -1,1 +1,45 @@
-print("hello world")
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) #this will enable cors for all the routes
+
+#in memory storage for notes
+
+notes = []
+note_id = 1
+
+
+
+@app.route("/notes",methods=["POST"])
+def add_note():
+    global note_id
+    data = request.json
+    if "title" not in data or "content" not in data:
+        return jsonify({"error" : "Title and Content are required"}), 400
+    
+    note = {"id": note_id, "title": data["title"], "content": data["content"]}
+    notes.append(note)
+    note_id +=1 
+    return jsonify(note), 201
+
+
+@app.route("/notes", methods=["GET"])
+def get_notes():
+    return jsonify(notes)
+
+
+@app.route("/notes/<int:note_id>", methods=["DELETE"])
+def delete_note(note_id):
+    global notes
+    notes  = [note for note in notes if note["id"] != note_id] 
+    return jsonify({"message" : "Note deleted"}), 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+    
+
--- dpa-ui/src/App.jsx
+++ dpa-ui/src/App.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState, useEffect} from "react";
+import axios from "axios"
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(){
+
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [error,setError] = useState("");
+
+
+  useEffect(() =>{
+    fetchNote();
+  },[])
+
+  const fetchNote = async () => {
+    try {
+      const response = await axios.get(`http://127.0.0.1:5000/notes`);
+      setNotes(response.data);
+    }catch(error){
+      setError("Failed to fetch notes.")
+    }
+  }
+
+
+  const deleteNote = async (id) => {
+    try {
+      await axios.delete(`http://127.0.0.1:5000/notes/${id}`);
+      setNotes(notes.filter(note => note.id !== id));
+    }catch(error){
+      setError("Failed to delete note.")
+    }
+  }
+
+  const addNote = async () => {
+
+    if (!title || !content){
+      setError("Both field  are required")
+      return;
+    }
+
+    try {
+
+      const response = await axios.post("http://127.0.0.1:5000/notes", {title,content});
+      setNotes([...notes,response.data]);
+      setTitle("");
+      setContent("");
+      setError("");
+
+
+    }catch(error){
+      setError("failed to add note.")
+    }
+  }
 
   return (
-    <>
+
+    <div style={{padding: "20px"}}>
+
+      <h2>Notes App</h2>
+
+      {error && <p style={{ color :"red" }}> {error} </p> }
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <br />
+      <div>
+
+      <textarea placeholder="Content" value={content} onChange={(e) => setContent(e.target.value)} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <br />
+      <div>
+      
+        <button onClick={addNote}> Add Note </button>
+      </div>
+      <br />
+
+      <ul>
+         {notes.map((note) => (
+          <li key={note.id}> <strong>{note.title}</strong> : {note.content}
+          <button onClick={()=> deleteNote(note.id)}> Delete </button>
+          </li>
+         ))}  
+      </ul>
+      
+    </div>
   )
 }
 
-export default App
+
+export default App;
